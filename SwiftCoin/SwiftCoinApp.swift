d38e9b2dbbff4180080a44a807f7f54d85563f05
Cem Telliagaoglu <cem.telliagaoglu@gmail.com>
2023-07-25 19:24:17 +0300

    Initial Commit

---
+++ SwiftCoin/SwiftCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCoinApp.swift
+//  SwiftCoin
+//
+//  Created by admin on 25.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
