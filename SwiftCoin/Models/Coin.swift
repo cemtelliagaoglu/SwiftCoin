7b862959bf47d1c756b70b3ce2748342870ecb16
Cem Telliagaoglu <cem.telliagaoglu@gmail.com>
2023-07-26 13:18:36 +0300

    Update Coin Model

--- SwiftCoin/Models/Coin.swift
+++ SwiftCoin/Models/Coin.swift
@@ -8,19 +8,31 @@
 import Foundation
 
 struct Coin: Codable, Identifiable {
-
     let id: String
     let name: String
     let symbol: String
     let image: String
-    let currentPrice: Double?
-    let marketRank: Int
-    let priceChangePercentage: Double
+    let currentPrice, totalVolume: Double
+    let marketRank, marketCap: Int
+    let marketCapChange, priceChange24H,priceChangePercentage, high24H, low24H, marketCapChangePercentage: Double
+    let sparkLineIn7D: SparklineIn7D
 
     enum CodingKeys: String, CodingKey {
         case name, symbol, image, id
+        case marketCap = "market_cap"
+        case marketCapChange = "market_cap_change_24h"
+        case marketCapChangePercentage = "market_cap_change_percentage_24h"
+        case high24H = "high_24h"
+        case low24H = "low_24h"
+        case priceChange24H = "price_change_24h"
+        case totalVolume = "total_volume"
         case currentPrice = "current_price"
         case marketRank = "market_cap_rank"
         case priceChangePercentage = "price_change_percentage_24h"
+        case sparkLineIn7D = "sparkline_in_7d"
+    }
+
+    struct SparklineIn7D: Codable {
+        let price: [Double]
     }
 }
