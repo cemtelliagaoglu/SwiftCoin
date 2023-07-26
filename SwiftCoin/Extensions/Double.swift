b00d61253224016f082e450c4acdb0748e8d4447
Cem Telliagaoglu <cem.telliagaoglu@gmail.com>
2023-07-26 09:22:21 +0300

    Create Double extension

---
+++ SwiftCoin/Extensions/Double.swift
@@ -0,0 +1,38 @@
+//
+//  Double.swift
+//  SwiftCoin
+//
+//  Created by admin on 25.07.2023.
+//
+
+import Foundation
+
+extension Double {
+
+    private var currencyFormatter: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.usesGroupingSeparator = true
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        return formatter
+    }
+
+    private var numberFormatter: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        return formatter
+    }
+
+    func toCurrency() -> String {
+        return currencyFormatter.string(for: self) ?? "0.00"
+    }
+
+    func toPercentString() -> String {
+        guard let percentString = numberFormatter.string(for: self) else { return "0.00%"}
+        return percentString + "%"
+    }
+
+}
