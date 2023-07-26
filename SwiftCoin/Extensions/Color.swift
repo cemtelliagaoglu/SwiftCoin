3ae93cd3f4310ead5c927432339805c0ac821601
Cem Telliagaoglu <cem.telliagaoglu@gmail.com>
2023-07-26 13:15:22 +0300

    Create Color+Extension

---
+++ SwiftCoin/Extensions/Color.swift
@@ -0,0 +1,17 @@
+//
+//  Color.swift
+//  SwiftCoin
+//
+//  Created by admin on 26.07.2023.
+//
+
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let primaryTextColor = Color("PrimaryTextColor")
+    let itemBackgroundColor = Color("ItemBackgroundColor")
+}
