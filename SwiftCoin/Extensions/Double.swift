811fc9733f0c75d7186718ea1f4796bee0a15fa9
Cem Telliagaoglu <cem.telliagaoglu@gmail.com>
2023-07-26 16:50:57 +0300

    Update Double+Extension

--- SwiftCoin/Extensions/Double.swift
+++ SwiftCoin/Extensions/Double.swift
@@ -33,4 +33,32 @@ extension Double {
         guard let percentString = numberFormatter.string(for: self) else { return "0.00%" }
         return percentString + "%"
     }
+
+    func asNumberString() -> String {
+        return String(format: "%.2f", self)
+    }
+
+    func formatWithAbbreviations() -> String {
+        let num = abs(Double(self))
+        let sign = (self < 0) ? "-" : ""
+
+        switch num {
+        case 1_000_000_000_000...:
+            let formatted = num / 1_000_000_000_000
+            return "\(sign)\(formatted.asNumberString())Tr"
+        case 1_000_000_000...:
+            let formatted = num / 1_000_000_000
+            return "\(sign)\(formatted.asNumberString())Bn"
+        case 1_000_000...:
+            let formatted = num / 1_000_000
+            return "\(sign)\(formatted.asNumberString())M"
+        case 1000...:
+            let formatted = num / 1000
+            return "\(sign)\(formatted.asNumberString())K"
+        case 0...:
+            return asNumberString()
+        default:
+            return "\(sign)\(asNumberString())"
+        }
+    }
 }
